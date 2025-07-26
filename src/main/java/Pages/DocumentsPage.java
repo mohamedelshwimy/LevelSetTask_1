c0788e046964368a6aa07ad538577165488cc2ac
mohamedelshwimy <mohamedahmedebrahim97@gmail.com>
2025-07-26 17:14:29 +0300

    Add method to get the number of free documents

--- src/main/java/Pages/DocumentsPage.java
+++ src/main/java/Pages/DocumentsPage.java
@@ -1,11 +1,40 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DocumentsPage {
 
     private WebDriver driver;
     public DocumentsPage (WebDriver driver){
         this.driver = driver;
+    }
+
+    private By freeDocuments = By.xpath("//span[contains(.,'Free')]//ancestor::div[contains(@class,'panel-heading')]//div[@class='left']");
+
+    public List<String> getFreeDocumentsNames(){
+
+        List<String> freeDocs = new ArrayList<>();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(freeDocuments));
+
+        List<WebElement> docs = driver.findElements(freeDocuments);
+        System.out.println("Number of free documents -> " + docs.size());
+
+        for (WebElement doc : docs) {
+            String docName = doc.getText();
+            freeDocs.add(docName);
+            System.out.println("Added docName = " + docName);
+        }
+
+        return freeDocs;
     }
 }
--- src/test/java/Tests/GetDocumentsName.java
+++ src/test/java/Tests/GetDocumentsName.java
@@ -3,6 +3,7 @@ package Tests;
 import Base.BaseTest;
 import Pages.DocumentsPage;
 import Pages.HomePage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -12,10 +13,13 @@ public class GetDocumentsName extends BaseTest {
 
     @Test
     public void goToDocumentsPage() {
-        SoftAssert softAssert = new SoftAssert();
 
         documentsPage = homePage.clickGetPaidBtn();
 
-        softAssert.assertAll();
+        int numberOfFreeDocs = documentsPage.getFreeDocumentsNames().size();
+        System.out.println("numberOfFreeDocs = " + numberOfFreeDocs);
+
+        Assert.assertEquals(numberOfFreeDocs, 2, "Incorrect Free Docs number");
+
     }
 }
