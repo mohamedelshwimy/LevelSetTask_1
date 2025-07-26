414512c139c7743f206bb35f25f519c925fc1b28
mohamedelshwimy <mohamedahmedebrahim97@gmail.com>
2025-07-26 17:32:30 +0300

    Add method to get the number of free documents

--- src/main/java/Pages/DocumentsPage.java
+++ src/main/java/Pages/DocumentsPage.java
@@ -17,9 +17,11 @@ public class DocumentsPage {
         this.driver = driver;
     }
 
+    //Locators
     private By freeDocuments = By.xpath("//span[contains(.,'Free')]//ancestor::div[contains(@class,'panel-heading')]//div[@class='left']");
 
-    public List<String> getFreeDocumentsNames(){
+    //Methods
+    public List<String> getFreeDocumentsNames() {
 
         List<String> freeDocs = new ArrayList<>();
 
@@ -34,7 +36,37 @@ public class DocumentsPage {
             freeDocs.add(docName);
             System.out.println("Added docName = " + docName);
         }
+        return freeDocs;
+    }
+
+    //Handle Exceptions
+    public List<String> getFreeDocumentsNames_2() {
+
+        try {
+
+            return getFreeDocuments();
+        } catch (Exception e) {
+            System.err.println("Unexpected error: " + e.getMessage());
+            driver.navigate().refresh();
+
+            return getFreeDocuments();
+        }
+    }
+
+    List<String> getFreeDocuments() {
+        List<String> freeDocs = new ArrayList<>();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(freeDocuments));
+        List<WebElement> docs = driver.findElements(freeDocuments);
+        System.out.println("Number of free documents: " + docs.size());
+
+        for (WebElement doc : docs) {
+            String docName = doc.getText().trim();
+            freeDocs.add(docName);
+        }
 
         return freeDocs;
     }
+
 }
